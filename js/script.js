02fa7afa8405ca85b87aa59aea646d481ad258f5
896ma <solarcrash3@gmail.com.com>
2023-08-28 17:19:21 +0300

    dist

--- js/script.js
+++ js/script.js
@@ -7,10 +7,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-/*==================== scroll sections active link ====================*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
