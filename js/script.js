2ce378bbbe620856d1c73d8d369fb50a4104a577
896ma <solarcrash3@gmail.com.com>
2023-08-30 21:01:08 +0300

    merged

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {SelectorAll('header nav a');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
